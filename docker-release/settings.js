2ee5a7aef149e1741f43643f8ab464978fba604a
ionutcarp <61273527+ionutcarp@users.noreply.github.com>
2021-10-22 18:25:46 +0300

    Custom v.2.1.1

--- docker-release/settings.js
+++ docker-release/settings.js
@@ -297,7 +297,7 @@ module.exports = {
       * will install/load. It can use '*' as a wildcard that matches anything.
       */
      externalModules: {
-         autoInstall: false,   /** Whether the runtime will attempt to automatically install missing modules */
+         autoInstall: true,   /** Whether the runtime will attempt to automatically install missing modules */
          autoInstallRetry: 30, /** Interval, in seconds, between reinstall attempts */
          palette: {              /** Configuration for the Palette Manager */
              allowInstall: true, /** Enable the Palette Manager in the editor */
